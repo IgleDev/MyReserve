bff4f705fd0eaa3cd01f7659c4de822f5b076a62
IgleDev <adriiglesias2016@gmail.com>
2024-06-23 20:39:32 +0200

    Añadido Repository del Grupo de Peluquerias

---
+++ MyReserve/Models/Repository/RepositoryGrupoPeluquerias/GrupoPeluqueriasRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueria;
+
+namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
+    public class GrupoPeluqueriasRepository : IGrupoPeluquerias{
+        private readonly Conexion _conexion;
+        public GrupoPeluqueriasRepository(Conexion conexion) {
+            _conexion = conexion;
+        }
+
+        public IEnumerable<Peluqueria> GetPeluquerias(GrupoPeluqueria gp) {
+            var query = "SELECT pelu.pelu_nombre FROM Peluqueria AS pelu " +
+                "INNER JOIN GrupoPeluqueria AS gp ON gp.gp_id = pelu.pelu_gp_id_fk " +
+                "WHERE pelu_gp_id_fk = @gp_id";
+
+            using(var connection = _conexion.getConexion()) {
+                var peluquerias = connection.Query<Peluqueria>(query, new { gp.gp_id });
+                return peluquerias;
+            }
+        }
+    }
+}
---
+++ MyReserve/Models/Repository/RepositoryGrupoPeluquerias/IGrupoPeluquerias.cs
@@ -0,0 +1,8 @@
+﻿using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueria;
+
+namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
+    public interface IGrupoPeluquerias {
+        IEnumerable<Peluqueria> GetPeluquerias(GrupoPeluqueria gp);
+    }
+}
