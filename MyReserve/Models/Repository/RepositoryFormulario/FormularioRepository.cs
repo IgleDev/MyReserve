aab27c804cee89159440cbca8095e9bb87dda6cb
IgleDev <adriiglesias2016@gmail.com>
2024-06-23 02:17:59 +0200

    Interfaces creadas y hechas para el uso del login y registro del formulario

--- MyReserve/Models/Repository/RepositoryFormulario/FormularioRepository.cs
+++ MyReserve/Models/Repository/RepositoryFormulario/FormularioRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Dapper;
+using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueria;
 using MyReserve.Models.TablasBBDD.Peluqueros;
 using MyReserve.Models.TablasBBDD.Usuarios;
 using System.Data;
@@ -9,6 +11,8 @@ namespace MyReserve.Models.Repository.RepositoryUsuarios {
         public FormularioRepository(Conexion conexion) {
             _conexion = conexion;
         }
+
+        // Logins ->
 
         public async Task<Usuarios> Login(string usu_correo_electronico, string usu_contrasenha) {
             var query = "SELECT * FROM Usuarios WHERE usu_correo_electronico = @usu_correo_electronico " +
@@ -28,6 +32,27 @@ namespace MyReserve.Models.Repository.RepositoryUsuarios {
             }
         }
 
+        public async Task<Peluqueria> LoginPeluqueria(string pelu_correo_electronico, string pelu_contrasenha) {
+            var query = "SELECT * FROM Peluqueria WHERE pelu_correo_electronico = @pelu_correo_electronico " +
+                "AND pelu_contrasenha = @pelu_contrasenha";
+            using(var connection = _conexion.getConexion()) {
+                var peluqueria = await connection.QueryFirstOrDefaultAsync<Peluqueria>(query, new { pelu_correo_electronico, pelu_contrasenha });
+                return peluqueria;
+            }
+        }
+
+        public async Task<GrupoPeluqueria> LoginGrupo(string gp_correo_electronico, string gp_contrasenha) {
+            var query = "SELECT * FROM GrupoPeluqueria WHERE gp_correo_electronico = @gp_correo_electronico " +
+                "AND gp_contrasenha = @gp_contrasenha";
+
+            using(var connection = _conexion.getConexion()) {
+                var grupoPeluqueria = await connection.QueryFirstOrDefaultAsync<GrupoPeluqueria>(query, new { gp_correo_electronico, gp_contrasenha });
+                return grupoPeluqueria;
+            }
+        }
+
+        // Registros ->
+
         public async Task RegistroUsuario(Usuarios usuarios) {
             var query = "INSERT INTO Usuarios (usu_nombre, usu_correo_electronico, usu_contrasenha, usu_pais, usu_ciudad) " +
                 "VALUES (@usu_nombre, @usu_correo_electronico, @usu_contrasenha, @usu_pais, @usu_ciudad)";
@@ -43,25 +68,11 @@ namespace MyReserve.Models.Repository.RepositoryUsuarios {
             }
         }
 
-        public async Task<int> ObtenerPeluqueriaIdPorNombreAsync(string nombre) {
-            var query = "SELECT pelu_id FROM Peluqueria WHERE pelu_nombre = @nombre";
-            using(var connection = _conexion.getConexion()) {
-                return await connection.ExecuteScalarAsync<int>(query, new { Nombre = nombre });
-            }
-        }
-
-        public async Task<int> ObtenerGrupoIdPorNombreAsync(string nombre) {
-            var query = "SELECT gp_id FROM GrupoPeluqueria WHERE gp_nombre = @nombre";
-            using(var connection = _conexion.getConexion()) {
-                return await connection.ExecuteScalarAsync<int>(query, new { Nombre = nombre });
-            }
-        }
-
         public async Task RegistroPeluqueros(Peluqueros peluqueros) {
-            var query = @"INSERT INTO Peluquero (pel_nombre, pel_correo_electronico, pel_contrasenha, pel_experiencia, 
-            pel_instagram, pel_pelu_id_fk, pel_grupo_id_fk) VALUES 
-            (@pel_nombre, @pel_correo_electronico, @pel_contrasenha, @pel_experiencia, @pel_instagram, 
-            @pel_pelu_id_fk, @pel_grupo_id_fk)";
+            var query = "INSERT INTO Peluquero (pel_nombre, pel_correo_electronico, pel_contrasenha, pel_experiencia, " +
+            "pel_instagram, pel_pelu_id_fk, pel_grupo_id_fk) VALUES " +
+            "(@pel_nombre, @pel_correo_electronico, @pel_contrasenha, @pel_experiencia, @pel_instagram, " +
+            "@pel_pelu_id_fk, @pel_grupo_id_fk)";
 
             var parametros = new DynamicParameters();
             parametros.Add("pel_nombre", peluqueros.pel_nombre, DbType.String);
@@ -74,6 +85,54 @@ namespace MyReserve.Models.Repository.RepositoryUsuarios {
 
             using(var connection = _conexion.getConexion()) {
                 await connection.ExecuteAsync(query, parametros);
+            }
+        }
+
+        public async Task RegistroPeluqueria(Peluqueria peluqueria) {
+            var query = "INSERT INTO Peluqueria (pelu_nombre, pelu_correo_electronico, pelu_contrasenha, pelu_pais, " +
+                "pelu_ciudad, pelu_telefono, pelu_gp_id_fk) VALUES (@pelu_nombre, @pelu_correo_electronico, @pelu_contrasenha, " +
+                "@pelu_pais, @pelu_ciudad, @pelu_telefono, @pelu_gp_id_fk)";
+
+            var parametros = new DynamicParameters();
+            parametros.Add("pelu_nombre", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_correo_electronico", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_contrasenha", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_pais", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_ciudad", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_telefono", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_gp_id_fk", peluqueria.pelu_nombre, DbType.String);
+
+            using(var connection = _conexion.getConexion()) {
+                await connection.ExecuteAsync(query, parametros);
+            }
+        }
+
+        public async Task RegistroGrupos(GrupoPeluqueria grupo) {
+            var query = "INSERT INTO GrupoPeluqueria (gp_nombre, gp_correo_electronico, gp_contrasenha) VALUES " +
+                "(@gp_nombre, @gp_correo_electronico, @gp_contrasenha)";
+
+            var parametros = new DynamicParameters();
+            parametros.Add("gp_nombre", grupo.gp_nombre, DbType.String);
+            parametros.Add("gp_correo_electronico", grupo.gp_correo_electronico, DbType.String);
+            parametros.Add("gp_contrasenha", grupo.gp_contrasenha, DbType.String);
+
+            using(var connection = _conexion.getConexion()) {
+                await connection.ExecuteAsync(query, parametros);
+            }
+        }
+
+        // Helpers de Querys
+        public async Task<int> PeluqueriaIDNombre(string pelu_nombre) {
+            var query = "SELECT pelu_id FROM Peluqueria WHERE pelu_nombre = @pelu_nombre";
+            using(var connection = _conexion.getConexion()) {
+                return await connection.ExecuteScalarAsync<int>(query, new { pelu_nombre });
+            }
+        }
+
+        public async Task<int> GrupoIdNombre(string gp_nombre) {
+            var query = "SELECT gp_id FROM GrupoPeluqueria WHERE gp_nombre = @gp_nombre";
+            using(var connection = _conexion.getConexion()) {
+                return await connection.ExecuteScalarAsync<int>(query, new { gp_nombre });
             }
         }
     }
--- MyReserve/Models/Repository/RepositoryFormulario/IFormulario.cs
+++ MyReserve/Models/Repository/RepositoryFormulario/IFormulario.cs
@@ -1,13 +1,25 @@
-﻿using MyReserve.Models.TablasBBDD.Peluqueros;
+﻿using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueros;
 using MyReserve.Models.TablasBBDD.Usuarios;
 
 namespace MyReserve.Models.Repository.RepositoryUsuarios {
     public interface IFormulario {
+
+        // Logins ->
         Task<Usuarios> Login(string usu_correo_electronico, string usu_contrasenha);
-        Task<Peluqueros> LoginPeluquero(string usu_correo_electronico, string usu_contrasenha);
+        Task<Peluqueros> LoginPeluquero(string pel_correo_electronico, string pel_contrasenha);
+        Task<Peluqueria> LoginPeluqueria(string pelu_correo_electronico, string pelu_contrasenha);
+        Task<GrupoPeluqueria> LoginGrupo(string gp_correo_electronico, string gp_contrasenha);
+
+        // Registros ->
         Task RegistroUsuario(Usuarios usuarios);
-        Task<int> ObtenerPeluqueriaIdPorNombreAsync(string nombre);
-        Task<int> ObtenerGrupoIdPorNombreAsync(string nombre);
         Task RegistroPeluqueros(Peluqueros peluqueros);
+        Task RegistroPeluqueria(Peluqueria peluqueria);
+        Task RegistroGrupos(GrupoPeluqueria grupo);
+
+        // Helpers de Querys
+        Task<int> PeluqueriaIDNombre(string pelu_nombre);
+        Task<int> GrupoIdNombre(string gp_nombre);
     }
 }
