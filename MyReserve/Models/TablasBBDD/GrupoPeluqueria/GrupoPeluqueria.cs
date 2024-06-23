318b2f30117cc9e7e70e14d84b1b5c63174489ab
IgleDev <adriiglesias2016@gmail.com>
2024-06-23 20:38:05 +0200

    Tabla del BBDD

--- MyReserve/Models/TablasBBDD/GrupoPeluqueria/GrupoPeluqueria.cs
+++ MyReserve/Models/TablasBBDD/GrupoPeluqueria/GrupoPeluqueria.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
+﻿
 namespace MyReserve.Models.TablasBBDD.GrupoPeluqueria {
     public class GrupoPeluqueria {
         public int gp_id { get; set; }
         public string gp_nombre { get; set; }
         public string gp_correo_electronico { get; set; }
         public string gp_contrasenha{ get; set; }
+        public IEnumerable<MyReserve.Models.TablasBBDD.Peluqueria.Peluqueria> peluquerias { get; set; }
     }
 }
