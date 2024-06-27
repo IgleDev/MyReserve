24293bbc58b9a166aaaca2127c2f56670e71e030
IgleDev <adriiglesias2016@gmail.com>
2024-06-27 03:22:00 +0200

    Añadido el peluqueria controller para mostrar las vistas de la carpeta

---
+++ MyReserve/Controllers/PeluqueriasController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyReserve.Models.Repository.RepositoryPeluqueria;
+using MyReserve.Models.Repository.RepositoryUsuarios;
+using MyReserve.Models.TablasBBDD.Peluqueria;
+using Newtonsoft.Json;
+
+namespace MyReserve.Controllers {
+    public class PeluqueriasController : Controller {
+        private readonly IPeluqueria _peluqueriasRepository;
+        public PeluqueriasController(IPeluqueria peluqueriasRepository) {
+            _peluqueriasRepository = peluqueriasRepository;
+        }
+
+        public IActionResult Portal() {
+            Peluqueria peluActual = deserializarPeluqueria();
+            var peluqueros = _peluqueriasRepository.GetPeluqueros(peluActual);
+            var grupoPeluqueros = _peluqueriasRepository.GrupoIdNombre(peluActual.pelu_gp_id_fk);
+            peluActual.peluqueros = peluqueros;
+            peluActual.grupoPeluqueria = grupoPeluqueros;
+            serializarPeluqueria(peluActual);
+            return View(peluActual); 
+        }
+
+        public void serializarPeluqueria(Peluqueria peluqueria) {
+            string json = JsonConvert.SerializeObject(peluqueria);
+            HttpContext.Session.SetString("PeluqueriaActual", json);
+        }
+
+        public Peluqueria deserializarPeluqueria() {
+            Peluqueria peluqueria = new Peluqueria();
+            string json = HttpContext.Session.GetString("PeluqueriaActual");
+            peluqueria = JsonConvert.DeserializeObject<Peluqueria>(json);
+            return peluqueria;
+        }
+
+    }
+}
