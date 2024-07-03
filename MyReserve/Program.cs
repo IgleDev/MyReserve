6f3db3ddba0cf2309933f7924f7387680cbbc0ed
IgleDev <adriiglesias2016@gmail.com>
2024-07-03 13:50:31 +0200

    Añadimos el Repository al archivo para poder trabajar con él

--- MyReserve/Program.cs
+++ MyReserve/Program.cs
@@ -2,6 +2,7 @@ using MyReserve.Models.Repository.RepositoryUsuarios;
 using MyReserve.Models;
 using MyReserve.Models.Repository.RepositoryGrupoPeluquerias;
 using MyReserve.Models.Repository.RepositoryPeluqueria;
+using MyReserve.Models.Repository.RepositoryUsuario;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionSt
 builder.Services.AddScoped<IFormulario, FormularioRepository>();
 builder.Services.AddScoped<IGrupoPeluquerias, GrupoPeluqueriasRepository>();
 builder.Services.AddScoped<IPeluqueria, PeluqueriaRepository>();
+builder.Services.AddScoped<IInfoUsuarios, IInfoUsuariosRepository>();
 builder.Services.AddSession();
 
 var app = builder.Build();
