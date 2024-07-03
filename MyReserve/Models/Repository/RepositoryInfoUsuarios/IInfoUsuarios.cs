99b4ec55d65b8345124dd44ec401105a61b19295
IgleDev <adriiglesias2016@gmail.com>
2024-07-03 13:49:01 +0200

    Creación de un Repository para la información del usuario

---
+++ MyReserve/Models/Repository/RepositoryInfoUsuarios/IInfoUsuarios.cs
@@ -0,0 +1,9 @@
+﻿using MyReserve.Models.TablasBBDD.Peluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueros;
+using MyReserve.Models.TablasBBDD.Usuarios;
+
+namespace MyReserve.Models.Repository.RepositoryUsuario {
+    public interface IInfoUsuarios {
+        IEnumerable<Peluqueros> GetPeluqueros(Usuarios usu);
+    }
+}
---
+++ MyReserve/Models/Repository/RepositoryInfoUsuarios/IInfoUsuariosRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyReserve.Models.TablasBBDD.Peluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueros;
+using MyReserve.Models.TablasBBDD.Usuarios;
+
+namespace MyReserve.Models.Repository.RepositoryUsuario {
+    public class IInfoUsuariosRepository : IInfoUsuarios{
+        private readonly Conexion _conexion;
+
+        public IInfoUsuariosRepository(Conexion conexion) {
+            _conexion = conexion;
+        }
+
+        public IEnumerable<Peluqueros> GetPeluqueros(Usuarios usu) {
+            throw new NotImplementedException();
+        }
+    }
+}
