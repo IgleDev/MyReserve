3ae7bc4a76e2bc9fc9cef8b81a917b3dc25c9e9e
IgleDev <adriiglesias2016@gmail.com>
2024-07-06 01:36:19 +0200

    Creacion de las clases de Paises y Regiones

---
+++ MyReserve/Models/TablasBBDD/Paises/Paises.cs
@@ -0,0 +1,6 @@
+﻿namespace MyReserve.Models.TablasBBDD.Paises{
+    public class Paises{
+        public int pai_id { get; set; }
+        public string pai_nombre {  get; set; }
+    }
+}
---
+++ MyReserve/Models/TablasBBDD/Region/Region.cs
@@ -0,0 +1,7 @@
+﻿namespace MyReserve.Models.TablasBBDD.Region {
+    public class Region {
+        public int reg_id { get; set; }
+        public int reg_pai_id_fk { get; set; }
+        public string reg_nombre { get; set; }
+    }
+}
