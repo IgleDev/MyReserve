8615f3ead8b407c35c261e764f87abaf7d5d6dd6
IgleDev <adriiglesias2016@gmail.com>
2024-07-19 20:24:00 +0200

    Modificacion del InfoUsuario para poder mostrar las regiones de los países

--- MyReserve/wwwroot/js/infousuarios/InfoUsuarios.js
+++ MyReserve/wwwroot/js/infousuarios/InfoUsuarios.js
@@ -1,3 +1,24 @@
 ﻿$(document).ready(function () {
-    
-}
+    $('#listaPais').change(function () {
+        let pai_nombre = $(this).val();
+        let regiones = $('#listaRegiones');
+        regiones.empty();
+        regiones.append('<option disabled value="">Seleccionar Región</option>');
+        if (pai_nombre) {
+            $.ajax({
+                url: '/InfoUsuarios/getRegionesPais',
+                type: 'GET',
+                data: { pai_nombre: pai_nombre },
+                success: function (data) {
+                    console.log(data);
+                    $.each(data, function (index, reg) {
+                        regiones.append(`<option value="${reg.reg_nombre}">${reg.reg_nombre}</option>`);
+                    });
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', error);
+                }
+            });
+        }
+    });
+});
