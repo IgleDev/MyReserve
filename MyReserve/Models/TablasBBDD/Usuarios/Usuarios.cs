07f610e68968c4ace7c2957a98da8a8a5802790b
IgleDev <adriiglesias2016@gmail.com>
2024-07-20 21:00:17 +0200

    Añadido una lista de peluquerias

--- MyReserve/Models/TablasBBDD/Usuarios/Usuarios.cs
+++ MyReserve/Models/TablasBBDD/Usuarios/Usuarios.cs
@@ -4,6 +4,7 @@
         public string? usu_nombre { get; set; }
         public string? usu_correo_electronico { get; set; }
         public string? usu_contrasenha { get; set; }
-        public IEnumerable<MyReserve.Models.TablasBBDD.Paises.Paises> listaPaises { get; set; }
+        public IEnumerable<MyReserve.Models.TablasBBDD.Peluqueria.Peluqueria> listaPeluqueria { get; set; } // Lista de Peluquerias del usuario.
+        public IEnumerable<MyReserve.Models.TablasBBDD.Paises.Paises> listaPaises { get; set; } // Lista de Paises que tendrá el usuario.
     }
 }
