301442ce8003988d0f12ce9c57297cbf296d653f
IgleDev <adriiglesias2016@gmail.com>
2024-07-22 03:35:58 +0200

    Modificacion del archivo js del usuario

--- MyReserve/wwwroot/js/infousuarios/InfoUsuarios.js
+++ MyReserve/wwwroot/js/infousuarios/InfoUsuarios.js
@@ -1,16 +1,17 @@
 ﻿$(document).ready(function () {
-    $('#listaPais').change(function () {    // Cada vez que se cambie de país se actualizará la información
+    $('#listaPais').change(function () { // Cada vez que se cambie de país se actualizará la información
         let pai_nombre = $(this).val(); // Recogemos el nombre del país
         let regiones = $('#listaRegiones'); // Recogemos la lista de las regiones
-        regiones.empty();   // Vaciamos los options del select
+        regiones.empty();    // Vaciamos los options del select
         regiones.append('<option disabled value="">Seleccionar Región</option>'); // Añadimos la línea de default
+
         if (pai_nombre) {   // Si hay nombre del país hacemos lo siguiente
             $.ajax({    // Función ajax
-                url: '/InfoUsuarios/getRegionesPais',   // Pasamos la URL que tenemos en el controller
+                url: '/Formularios/getRegionesPais',    // Pasamos la URL que tenemos en el controller
                 type: 'GET',    // Tipo GET
-                data: { pai_nombre: pai_nombre }, // Se envía el nombre del país como parámetro de consulta
+                data: { pai_nombre: pai_nombre },   // Se envía el nombre del país como parámetro de consulta
                 success: function (data) {  // Si la operación es exitosa (Código 200)
-                    $.each(data, function (reg) {    // Bucle foreach que nos muestre los nombres de las regiones
+                    $.each(data, function (index, reg) {    // Bucle foreach que nos muestre los nombres de las regiones
                         regiones.append(`<option value="${reg.reg_nombre}">${reg.reg_nombre}</option>`);    // Las añadimos
                     });
                 },
