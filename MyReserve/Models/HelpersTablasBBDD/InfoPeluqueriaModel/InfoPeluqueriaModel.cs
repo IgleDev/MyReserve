5e6f77d831e77a9993b7a3deb79118b0e71b30c5
IgleDev <adriiglesias2016@gmail.com>
2024-07-22 03:34:22 +0200

    Creación de una carpeta helper para ayudar a la BBDD para poder juntas tablas

---
+++ MyReserve/Models/HelpersTablasBBDD/InfoPeluqueriaModel/InfoPeluqueriaModel.cs
@@ -0,0 +1,8 @@
+﻿using MyReserve.Models.TablasBBDD.Peluqueria;
+
+namespace MyReserve.Models.HelpersTablasBBDD.InfoPeluqueriaModel {
+    public class InfoPeluqueriaModel {
+        public Peluqueria PeluqueriaInfo { get; set; }
+        public string UsuarioNombre { get; set; }
+    }
+}
