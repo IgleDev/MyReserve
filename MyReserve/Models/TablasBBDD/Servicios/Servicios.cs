492fa33d7f13c6f896558030ad471ee36ad16229
IgleDev <adriiglesias2016@gmail.com>
2024-07-24 21:58:50 +0200

    Añadido tabla de servicios de la BBDD

---
+++ MyReserve/Models/TablasBBDD/Servicios/Servicios.cs
@@ -0,0 +1,10 @@
+﻿namespace MyReserve.Models.TablasBBDD.Servicios {
+    public class Servicios {
+        public int ser_id {  get; set; }
+        public string? ser_nombre { get; set; }
+        public int ser_precio { get; set; }
+        public string? ser_pelu_id_fk { get; set; }
+        public string? ser_cat_id_fk { get; set; }
+        public string? cat_nombre { get; set; }
+    }
+}
