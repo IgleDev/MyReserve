1d7a9c4ec6b0dda75babeb5225a53cbf7bc06a40
IgleDev <adriiglesias2016@gmail.com>
2024-07-24 03:27:57 +0200

    Creación del archi de js para hacer más intuito al usuario que está seleccionando

---
+++ MyReserve/wwwroot/js/infousuarios/infoPeluquerias.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $('#peluqueros button').click(function () {
+        $('#peluqueros button').removeClass('btn-success').addClass('btn-outline-primary');
+        $(this).removeClass('btn-outline-primary').addClass('btn-success');
+    });
+});
