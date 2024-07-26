8412db7c6ffab10eba9452420f42354efc8ad478
IgleDev <adriiglesias2016@gmail.com>
2024-07-26 16:36:12 +0200

    Añadido un nuevo parametros para controlar los servicios

--- MyReserve/Models/TablasBBDD/Servicios/Servicios.cs
+++ MyReserve/Models/TablasBBDD/Servicios/Servicios.cs
@@ -5,6 +5,7 @@
         public int ser_precio { get; set; }
         public string? ser_pelu_id_fk { get; set; }
         public string? ser_cat_id_fk { get; set; }
+        public int ser_asociado { get; set; }
         public string? cat_nombre { get; set; }
     }
 }
