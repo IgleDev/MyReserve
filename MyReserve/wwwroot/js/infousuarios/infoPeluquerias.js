1de5b9dccbcb386e94eb9cbb568895901549c9b8
IgleDev <adriiglesias2016@gmail.com>
2024-08-07 16:56:07 +0200

    Modificacion de la vista para mostrar de forma correcta el datepicker

--- MyReserve/wwwroot/js/infousuarios/infoPeluquerias.js
+++ MyReserve/wwwroot/js/infousuarios/infoPeluquerias.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
-    $('#peluqueros button').click(function () {
-        $('#peluqueros button').removeClass('btn-success').addClass('btn-outline-primary');
-        $(this).removeClass('btn-outline-primary').addClass('btn-success');
-    });
+    var diaActualEntero = new Date(); // Cogemos la fecha de hoy
+    var anhoActual = diaActualEntero.getFullYear();   // Cogemos el año actual
+    var mesActual = String(diaActualEntero.getMonth() + 1).padStart(2, '0'); // Lo pasamos a String para poder mostrarlo y le sumamos uno porque Enero es el mes 0 y lo formateamos a 2 dígitos
+    var diaActual = String(diaActualEntero.getDate()).padStart(2, '0');   // Formateamos a 2 dígitos el dia actual
+    var fechaFinal = anhoActual + '-' + mesActual + '-' + diaActual;    // Montamos la fecha
+    $('#fechaCita').attr('min', fechaFinal);    // Mandamos la fehca atraves del campo min indicando que la fecha que introduzcamos tiene que ser menor
 });
