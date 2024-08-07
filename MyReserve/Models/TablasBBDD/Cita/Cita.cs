6d48b8ed55c9f6253e0f2d9432f3b9b2ab142d1d
IgleDev <adriiglesias2016@gmail.com>
2024-08-07 20:34:10 +0200

    Clase cita para las citas de los usuarios

---
+++ MyReserve/Models/TablasBBDD/Cita/Cita.cs
@@ -0,0 +1,11 @@
+﻿namespace MyReserve.Models.TablasBBDD.Cita {
+    public class Cita {
+        public int cita_id { get; set; }
+        public int cita_usu_id_fk { get; set; }
+        public int cita_pelu_id_fk { get; set; }
+        public int cita_pel_id_fk { get; set; }
+        public int cita_hora_id_fk { get; set; }
+        public DateTime cita_fecha { get; set; }
+        public List<int> servicios { get; set; }
+    }
+}
