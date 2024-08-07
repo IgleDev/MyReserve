fa130d876702a885de4c5658f5f6b3c5d628f5cc
IgleDev <adriiglesias2016@gmail.com>
2024-08-07 16:54:16 +0200

    Modificacion para la vista

--- MyReserve/Models/HelpersTablasBBDD/InfoPeluqueriaModel/InfoPeluqueriaModel.cs
+++ MyReserve/Models/HelpersTablasBBDD/InfoPeluqueriaModel/InfoPeluqueriaModel.cs
@@ -4,7 +4,7 @@ using MyReserve.Models.TablasBBDD.Peluqueros;
 namespace MyReserve.Models.HelpersTablasBBDD.InfoPeluqueriaModel {
     public class InfoPeluqueriaModel {
         public Peluqueria peluqueriaInfo { get; set; }
-        public IEnumerable<Peluqueros> peluqueroInfo { get; set; }
-        public string usu_nombre { get; set; }
+        public IEnumerable<Peluqueros> peluquerosInfo { get; set; }
+        public Peluqueros peluqueroInfo { get; set; }
     }
 }
