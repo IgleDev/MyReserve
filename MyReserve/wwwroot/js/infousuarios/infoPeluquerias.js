9c9c919624fe973bcaa00b3d3a5e0a8f0a5e5d8a
IgleDev <adriiglesias2016@gmail.com>
2024-08-07 20:35:05 +0200

    Creada una función AJAX para validar las horas disponibles dependiendo del día escogido por el usuario

--- MyReserve/wwwroot/js/infousuarios/infoPeluquerias.js
+++ MyReserve/wwwroot/js/infousuarios/infoPeluquerias.js
@@ -5,4 +5,40 @@
     var diaActual = String(diaActualEntero.getDate()).padStart(2, '0');   // Formateamos a 2 dígitos el dia actual
     var fechaFinal = anhoActual + '-' + mesActual + '-' + diaActual;    // Montamos la fecha
     $('#fechaCita').attr('min', fechaFinal);    // Mandamos la fehca atraves del campo min indicando que la fecha que introduzcamos tiene que ser menor
+
+    var pelu_id = $('#pelu_id').val(); // Verifica que esto esté inyectando el valor correcto
+
+    $('#fechaCita').change(function () {
+        var fechaCita = $(this).val();
+        console.log("Fecha de cita: " + fechaCita);
+
+        $.ajax({
+            url: '/InfoUsuarios/ObtenerHorariosDisponibles',
+            type: 'GET',
+            data: { pelu_id: pelu_id, fechaCita: fechaCita },
+            success: function (response) {
+                $('#horariosDisponibles').empty(); // Limpia los horarios actuales
+                if (!response || response.length === 0) {
+                    $('#horariosDisponibles').append(`<p>No hay horarios disponibles para esta fecha.</p>`);
+                    return;
+                }
+                $.each(response, function (index, horario) {
+                    $('#horariosDisponibles').append(
+                        `<div class="d-inline-flex flex-wrap mb-2">
+                                <div class="d-inline-flex flex-wrap mb-2">
+                                    <div class="form-check">
+                                        <input type="radio" class="btn-check" autocomplete="off" name="horariosPeluqueria"
+                                               value="${horario.hora_id}" id="btn-check-horarios-${horario.hora_id}" />
+                                        <label class="btn" for="btn-check-horarios-${horario.hora_id}">${horario.hora_fecha}</label>
+                                    </div>
+                                </div>
+                            </div>`
+                    );
+                });
+            },
+            error: function (xhr, status, error) {
+                console.error("Error al obtener los horarios disponibles:", error);
+            }
+        });
+    });
 });
