95045db8863ce677d8ff2285b7b556b0091bd95f
IgleDev <adriiglesias2016@gmail.com>
2024-08-13 23:38:07 +0200

    modifcaion

--- MyReserve/Models/TablasBBDD/Cita/Cita.cs
+++ MyReserve/Models/TablasBBDD/Cita/Cita.cs
@@ -7,5 +7,11 @@
         public int cita_hora_id_fk { get; set; }
         public DateTime cita_fecha { get; set; }
         public List<int> servicios { get; set; }
+        public string usu_nombre { get; set; }
+        public string pel_nombre { get; set; }
+        public string pelu_nombre { get; set; }
+        public TimeSpan hora_fecha { get; set; }
+        public string ser_nombres { get; set; }
+        public decimal ser_precio_total { get; set; }
     }
 }
