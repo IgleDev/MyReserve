8a8d717fa46c7290be1bba2ac06e59f4d9c8894d
IgleDev <adriiglesias2016@gmail.com>
2024-08-15 18:32:44 +0200

    Modificacion del repository para poder hacer las querys pertinentes para editar y eliminar peluquerias

--- MyReserve/Models/Repository/RepositoryGrupoPeluquerias/GrupoPeluqueriasRepository.cs
+++ MyReserve/Models/Repository/RepositoryGrupoPeluquerias/GrupoPeluqueriasRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Dapper;
 using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
 using MyReserve.Models.TablasBBDD.Peluqueria;
+using MyReserve.Models.TablasBBDD.Usuarios;
+using System.Data;
 
 namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
     public class GrupoPeluqueriasRepository : IGrupoPeluquerias{
@@ -10,13 +12,51 @@ namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
         }
 
         public IEnumerable<Peluqueria> GetPeluquerias(GrupoPeluqueria gp) {
-            var query = "SELECT pelu.pelu_nombre FROM Peluqueria AS pelu " +
+            var query = "SELECT pelu.* FROM Peluqueria AS pelu " +
                 "INNER JOIN GrupoPeluqueria AS gp ON gp.gp_id = pelu.pelu_gp_id_fk " +
                 "WHERE pelu_gp_id_fk = @gp_id";
 
             using(var conexion = _conexion.getConexion()) {
                 var peluquerias = conexion.Query<Peluqueria>(query, new { gp.gp_id });
                 return peluquerias;
+            }
+        }
+
+        public async Task<Peluqueria> getPeluqueriaID(int pelu_id) {
+            var query = "SELECT pelu.* FROM Peluqueria AS pelu " +
+                "WHERE pelu_id = @pelu_id";
+
+            using(var conexion = _conexion.getConexion()) {
+                var peluqueria = await conexion.QueryFirstOrDefaultAsync<Peluqueria>(query, new { pelu_id });
+                return peluqueria;
+            }
+        }
+
+        public async Task Editar(Peluqueria peluqueria) {
+            var query = "UPDATE Peluqueria SET pelu_nombre = @pelu_nombre, pelu_correo_electronico = @pelu_correo_electronico, " +
+                "pelu_contrasenha = @pelu_contrasenha, pelu_direccion = @pelu_direccion, pelu_telefono = @pelu_telefono " +
+                "WHERE pelu_id = @pelu_id";
+
+            var parametros = new DynamicParameters();
+            parametros.Add("pelu_nombre", peluqueria.pelu_nombre, DbType.String);
+            parametros.Add("pelu_correo_electronico", peluqueria.pelu_correo_electronico, DbType.String);
+            parametros.Add("pelu_contrasenha", peluqueria.pelu_contrasenha, DbType.String);
+            parametros.Add("pelu_direccion", peluqueria.pelu_direccion, DbType.String);
+            parametros.Add("pelu_telefono", peluqueria.pelu_telefono, DbType.Int32);
+            parametros.Add("pelu_id", peluqueria.pelu_id, DbType.Int32);
+
+            using(var conexion = _conexion.getConexion()) {
+                 await conexion.ExecuteAsync(query, parametros);
+            }
+        }
+
+        public async Task Eliminar(Peluqueria peluqueria) {
+            var query = "DELETE FROM Peluqueria WHERE pelu_id = @pelu_id";
+            var parametros = new DynamicParameters();
+            parametros.Add("pelu_id", peluqueria.pelu_id, DbType.Int32);
+
+            using(var conexion = _conexion.getConexion()) {
+                await conexion.ExecuteAsync(query, parametros);
             }
         }
     }
--- MyReserve/Models/Repository/RepositoryGrupoPeluquerias/IGrupoPeluquerias.cs
+++ MyReserve/Models/Repository/RepositoryGrupoPeluquerias/IGrupoPeluquerias.cs
@@ -4,5 +4,8 @@ using MyReserve.Models.TablasBBDD.Peluqueria;
 namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
     public interface IGrupoPeluquerias {
         IEnumerable<Peluqueria> GetPeluquerias(GrupoPeluqueria gp);
+        Task<Peluqueria> getPeluqueriaID(int pelu_id);
+        Task Editar(Peluqueria peluqueria);
+        Task Eliminar (Peluqueria peluqueria);
     }
 }
