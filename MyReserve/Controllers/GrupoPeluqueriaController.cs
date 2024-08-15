cfad2fbce975becc8351baa7e17223135de37088
IgleDev <adriiglesias2016@gmail.com>
2024-08-15 18:32:07 +0200

    Modificacion del controlador para poder Editar y Elimar Peluquerias

--- MyReserve/Controllers/GrupoPeluqueriaController.cs
+++ MyReserve/Controllers/GrupoPeluqueriaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyReserve.Models.Repository.RepositoryGrupoPeluquerias;
 using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
+using MyReserve.Models.TablasBBDD.Peluqueria;
 using Newtonsoft.Json;
 
 namespace MyReserve.Controllers {
@@ -16,6 +17,29 @@ namespace MyReserve.Controllers {
             grupoActual.peluquerias = grupoPeluquerias; // Añadimos las peluquerias a la lista del grupo
             serializarGrupo(grupoActual);   // Guardamos en sesión
             return View(grupoPeluquerias);  // Retornamos el grupo
+        }
+
+        public async Task<IActionResult> EditarPeluqueria(int pelu_id) {
+            GrupoPeluqueria grupoActual = deserializarGrupo();  // Recuperamos el grupo
+            ViewBag.grupoNombre = grupoActual.gp_nombre;
+            var peluEditar = await _grupoPeluqueriasRepository.getPeluqueriaID(pelu_id);
+            return View(peluEditar);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Peluqueria peluqueria) {
+            GrupoPeluqueria grupoActual = deserializarGrupo();
+            await _grupoPeluqueriasRepository.Editar(peluqueria);
+            var grupoPeluquerias = _grupoPeluqueriasRepository.GetPeluquerias(grupoActual); // Recuperamos las peluquerias del grupo
+            return View("Portal", grupoPeluquerias);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Eliminar(Peluqueria peluqueria) {
+            GrupoPeluqueria grupoActual = deserializarGrupo();
+            await _grupoPeluqueriasRepository.Eliminar(peluqueria);
+            var grupoPeluquerias = _grupoPeluqueriasRepository.GetPeluquerias(grupoActual); // Recuperamos las peluquerias del grupo
+            return View("Portal", grupoPeluquerias);
         }
 
         public void serializarGrupo(GrupoPeluqueria grupo) {
