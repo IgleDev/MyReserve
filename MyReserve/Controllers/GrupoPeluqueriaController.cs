849fa697bfb2d1ce0356983b2dec1f693393e053
IgleDev <adriiglesias2016@gmail.com>
2024-08-16 11:16:49 +0200

    Modificado el controlador para poder realizar acciones de editar y eliminar

--- MyReserve/Controllers/GrupoPeluqueriaController.cs
+++ MyReserve/Controllers/GrupoPeluqueriaController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyReserve.Models.Repository.RepositoryGrupoPeluquerias;
-using MyReserve.Models.Repository.RepositoryUsuarios;
 using MyReserve.Models.TablasBBDD.GrupoPeluqueria;
 using MyReserve.Models.TablasBBDD.Peluqueria;
-using MyReserve.Models.TablasBBDD.Usuarios;
 using Newtonsoft.Json;
 
 namespace MyReserve.Controllers {
@@ -15,11 +13,10 @@ namespace MyReserve.Controllers {
 
         public IActionResult Portal() {
             GrupoPeluqueria grupoActual = deserializarGrupo();  // Recuperamos el grupo
-            ViewBag.grupoID = grupoActual.gp_id;
             var grupoPeluquerias = _grupoPeluqueriasRepository.GetPeluquerias(grupoActual); // Recuperamos las peluquerias del grupo
             grupoActual.peluquerias = grupoPeluquerias; // Añadimos las peluquerias a la lista del grupo
             serializarGrupo(grupoActual);   // Guardamos en sesión
-            return View(grupoPeluquerias);  // Retornamos el grupo
+            return View(grupoActual);  // Retornamos el grupo
         }
 
         public async Task<IActionResult> EditarPeluqueria(int pelu_id) {
@@ -32,13 +29,13 @@ namespace MyReserve.Controllers {
         [HttpPost]
         public async Task<IActionResult> Editar(Peluqueria peluqueria) {
             GrupoPeluqueria grupoActual = deserializarGrupo();  // Recuperamos el grupo
-            var peluCorreo = await _grupoPeluqueriasRepository.getPeluqueriaID(peluqueria.pelu_id);
+            var peluCorreo = await _grupoPeluqueriasRepository.getPeluqueriaID(peluqueria.pelu_id); // Recuperamos el correo actual del grupo
 
-            if(peluCorreo.pelu_correo_electronico != peluqueria.pelu_correo_electronico) {
-                var correo = await _grupoPeluqueriasRepository.comprobarCorreoPeluquerias(peluqueria.pelu_correo_electronico);
-                if(correo) {
-                    ModelState.AddModelError("pelu_correo_electronico", "El correo electrónico ya está en uso.");
-                    return View("EditarPeluqueria", peluqueria);
+            if(peluCorreo.pelu_correo_electronico != peluqueria.pelu_correo_electronico) {  // Comparamos los correos
+                var correo = await _grupoPeluqueriasRepository.comprobarCorreoPeluquerias(peluqueria.pelu_correo_electronico);  //Comprobamos que no haya uno igual en la BBDD
+                if(correo) {    // Validamos
+                    ModelState.AddModelError("pelu_correo_electronico", "El correo electrónico ya está en uso.");   // Mensaje de error en caso de que el correo introducido está en la BBDD
+                    return View("EditarPeluqueria", peluqueria);    // Retornamos la vista
                 }
             }
 
@@ -58,6 +55,32 @@ namespace MyReserve.Controllers {
         public async Task<IActionResult> EditarGrupo(int gp_id) {
             var grupoEditar = await _grupoPeluqueriasRepository.getGrupoPeluqueria(gp_id);    // Recuperamos la peluqueria
             return View(grupoEditar);    // Retornamos la vista
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditarGP(GrupoPeluqueria gp) {
+            GrupoPeluqueria grupoActual = deserializarGrupo();  // Recuperamos el grupo
+            if (grupoActual.gp_correo_electronico != gp.gp_correo_electronico) {
+                var correo = await _grupoPeluqueriasRepository.comprobarCorreoGrupo(gp.gp_correo_electronico);
+                if(correo) {
+                    ModelState.AddModelError("gp_correo_electronico", "El correo electrónico ya está en uso.");   // Mensaje de error en caso de que el correo introducido está en la BBDD
+                    var grupoPeluqueriasError = _grupoPeluqueriasRepository.GetPeluquerias(gp); // Recuperamos las peluquerias del grupo
+                    gp.peluquerias = grupoPeluqueriasError;
+                    return View("EditarGrupo", gp);    // Retornamos la vista
+                }
+            }
+
+            await _grupoPeluqueriasRepository.EditarGrupo(gp);
+            serializarGrupo(gp);
+            var grupoPeluquerias = _grupoPeluqueriasRepository.GetPeluquerias(gp); // Recuperamos las peluquerias del grupo
+            gp.peluquerias = grupoPeluquerias;
+            return View("Portal", gp);    // Retornamos la vista
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EliminarGP(GrupoPeluqueria gp) {
+            await _grupoPeluqueriasRepository.EliminarGrupo(gp);
+            return RedirectToAction("Index", "Home");
         }
 
         public void serializarGrupo(GrupoPeluqueria grupo) {
