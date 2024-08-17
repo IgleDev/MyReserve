93d39eaeb7d1c76a1226c2546f42cd106ad0ba32
IgleDev <adriiglesias2016@gmail.com>
2024-08-17 13:47:50 +0200

    Modificacion de los métodos necesarios para eliminar una peluqueria y un grupo

--- MyReserve/Models/Repository/RepositoryGrupoPeluquerias/GrupoPeluqueriasRepository.cs
+++ MyReserve/Models/Repository/RepositoryGrupoPeluquerias/GrupoPeluqueriasRepository.cs
@@ -50,7 +50,7 @@ namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
         }
 
         public async Task Eliminar(Peluqueria peluqueria) {
-            var queryGetPeluquero = "SELECT pel_id FROM Peluquero WHERE pel_pelu_id_fk = @pelu_id";
+            var querygetPeluquero = "SELECT pel_id FROM Peluquero WHERE pel_pelu_id_fk = @pelu_id";
             var queryServiciosPeluqueria = "DELETE FROM PeluqueriaServicios WHERE pelu_ser_pelu_id_fk = @pelu_id";
             var queryPeluqueriaHorarios = "DELETE FROM PeluqueriaHorarios WHERE pelu_hora_pelu_id_fk = @pelu_id";
             var querygetCitas = "SELECT cita_id FROM Citas WHERE cita_pelu_id_fk = @pelu_id";
@@ -72,8 +72,11 @@ namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
                 await conexion.ExecuteAsync(queryCita, parametros);
                 await conexion.ExecuteAsync(queryServiciosPeluqueria, parametros);
 
-                var pel_id = await conexion.QueryFirstOrDefaultAsync<int?>(queryGetPeluquero, parametros);
-                await conexion.ExecuteAsync(queryPeluquero, new { pel_id });
+                var pel_ids = await conexion.QueryAsync<int?>(querygetPeluquero, parametros);
+                foreach(var pel_id in pel_ids) {
+                    await conexion.ExecuteAsync(queryPeluquero, new { pel_id });
+                }
+
                 await conexion.ExecuteAsync(queryPeluqueriaHorarios, parametros);
                 await conexion.ExecuteAsync(queryServicio, parametros);
                 await conexion.ExecuteAsync(queryPeluqueria, parametros);
@@ -121,6 +124,17 @@ namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
         }
 
         public async Task EliminarGrupo(GrupoPeluqueria gp) {
+            var querygetPeluquerias = "SELECT pelu_id FROM Peluqueria WHERE pelu_gp_id_fk = @gp_id";
+            var querygetPeluquero = "SELECT pel_id FROM Peluquero WHERE pel_grupo_id_fk = @gp_id";
+            var querygetCitasPeluqueria = "SELECT cita_id FROM Citas WHERE cita_pelu_id_fk = @pelu_id";
+            var querygetCitasPeluquero = "SELECT cita_id FROM Citas WHERE cita_pel_id_fk = @pel_id";
+            var querygetServiciosPeluqueria = "SELECT ser_id FROM Servicios WHERE ser_pelu_id_fk = @pelu_id";
+            var queryServiciosCita = "DELETE FROM CitasServicios WHERE citas_ser_cita_id_fk = @cita_id";
+            var queryCita = "DELETE FROM Citas WHERE cita_id = @cita_id";
+            var queryServiciosPeluqueria = "DELETE FROM Servicios WHERE ser_id = @ser_id";
+            var queryPeluqueriaServicios = "DELETE FROM PeluqueriaServicios WHERE pelu_ser_pelu_id_fk = @pelu_id";
+            var queryPeluqueriaHorarios = "DELETE FROM PeluqueriaHorarios WHERE pelu_hora_pelu_id_fk = @pelu_id";
+            var queryPeluquero = "DELETE FROM Peluquero WHERE pel_id = @pel_id";
             var queryPeluquerias = "DELETE FROM Peluqueria WHERE pelu_gp_id_fk = @gp_id";
             var queryGrupo = "DELETE FROM GrupoPeluqueria WHERE gp_id = @gp_id";
 
@@ -128,6 +142,34 @@ namespace MyReserve.Models.Repository.RepositoryGrupoPeluquerias {
             parametros.Add("gp_id", gp.gp_id, DbType.Int32);
 
             using(var conexion = _conexion.getConexion()) {
+                var peluquerias = await conexion.QueryAsync<int>(querygetPeluquerias, parametros);
+                foreach(var pelu_id in peluquerias) {
+                    var citasPeluqueria = await conexion.QueryAsync<int>(querygetCitasPeluqueria, new { pelu_id });
+                    foreach(var cita_id in citasPeluqueria) {
+                        await conexion.ExecuteAsync(queryServiciosCita, new { cita_id });
+                        await conexion.ExecuteAsync(queryCita, new { cita_id });
+                    }
+
+                    await conexion.ExecuteAsync(queryPeluqueriaServicios, new { pelu_id });
+
+                    var serviciosPeluqueria = await conexion.QueryAsync<int>(querygetServiciosPeluqueria, new { pelu_id });
+                    foreach(var ser_id in serviciosPeluqueria) {
+                        await conexion.ExecuteAsync(queryServiciosPeluqueria, new { ser_id });
+                    }
+
+                    await conexion.ExecuteAsync(queryPeluqueriaHorarios, new { pelu_id });
+                }
+
+                var peluqueros = await conexion.QueryAsync<int>(querygetPeluquero, parametros);
+                foreach(var pel_id in peluqueros) {
+                    var citasPeluquero = await conexion.QueryAsync<int>(querygetCitasPeluquero, new { pel_id });
+                    foreach(var cita_id in citasPeluquero) {
+                        await conexion.ExecuteAsync(queryServiciosCita, new { cita_id });
+                        await conexion.ExecuteAsync(queryCita, new { cita_id });
+                    }
+                    await conexion.ExecuteAsync(queryPeluquero, new { pel_id });
+                }
+
                 await conexion.ExecuteAsync(queryPeluquerias, parametros);
                 await conexion.ExecuteAsync(queryGrupo, parametros);
             }
