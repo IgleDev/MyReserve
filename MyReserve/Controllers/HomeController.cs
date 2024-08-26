32616f2accbc10e1c9eaa1dc20814ca4fc41d0b8
IgleDev <adriiglesias2016@gmail.com>
2024-08-26 16:10:10 +0200

    Modificacion del controlador del Home, faltan errores por poner

--- MyReserve/Controllers/HomeController.cs
+++ MyReserve/Controllers/HomeController.cs
@@ -13,5 +13,17 @@ namespace MyReserve.Controllers {
         public IActionResult Index() {
             return View();  // Devolvemos la vista principal de la aplicaión
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int codigoError) {
+            switch(codigoError) {
+                case 404:
+                    return View("Error404");
+                case 405:
+                    return View("Error404");
+                default:
+                    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
     }
 }
