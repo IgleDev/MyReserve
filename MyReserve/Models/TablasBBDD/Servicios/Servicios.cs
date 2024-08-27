afa2c9ad76f40037baef00bd572ca260f75434f4
IgleDev <adriiglesias2016@gmail.com>
2024-08-27 16:03:34 +0200

    Modificacion de la clase servicios conviertendola en un decimal

--- MyReserve/Models/TablasBBDD/Servicios/Servicios.cs
+++ MyReserve/Models/TablasBBDD/Servicios/Servicios.cs
@@ -2,7 +2,7 @@
     public class Servicios {
         public int ser_id {  get; set; }
         public string? ser_nombre { get; set; }
-        public int ser_precio { get; set; }
+        public decimal ser_precio { get; set; }
         public string? ser_pelu_id_fk { get; set; }
         public string? ser_cat_id_fk { get; set; }
         public int ser_asociado { get; set; }
