9c2eeb5f18f120d4b3718953480e455c369cc936
IgleDev <adriiglesias2016@gmail.com>
2024-08-27 13:28:05 +0200

    Añadido un nuevo controlador para manejar los errores en vez del controlador Home

---
+++ MyReserve/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyReserve.Models.HelpersTablasBBDD.CodigoError;
+
+namespace MyReserve.Controllers {
+    public class ErrorController : Controller {
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int CodigoError) {
+            var errorModel = new CodigoError {  // Creamos la vista
+                codigoError = CodigoError
+            };
+
+            return View(errorModel);    // La mandamos
+        }
+    }
+}
