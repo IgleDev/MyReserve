2e009bf0d780fbd687b0797df7f8ab1872387974
IgleDev <adriiglesias2016@gmail.com>
2024-08-27 16:59:01 +0200

    Se quitó el método de error del controlador Home

--- MyReserve/Controllers/HomeController.cs
+++ MyReserve/Controllers/HomeController.cs
@@ -13,35 +13,5 @@ namespace MyReserve.Controllers {
         public IActionResult Index() {
             return View();  // Devolvemos la vista principal de la aplicaión
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error(int codigoError) {
-            switch(codigoError) {
-                case 400:
-                    return View("Error400");
-                case 401:
-                    return View("Error401");
-                case 403:
-                    return View("Error403");
-                case 404:
-                    return View("Error404");
-                case 405:
-                    return View("Error405");
-                case 500:
-                    return View("Error500");
-                case 502:
-                    return View("Error502");
-                case 503:
-                    return View("Error503");
-                case 504:
-                    return View("Error504");
-                case 505:
-                    return View("Error505");
-                case 507:
-                    return View("Error507");
-                default:
-                    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-            }
-        }
     }
 }
